89c80c7846ce7d99b7b50b4011dd8f8b697df514
cjforman <chrisjforman@gmail.com>
2018-03-12 13:31:41 -0500

    Modified ellipsoid convertor

--- Projects/ellipseConv.py
+++ Projects/ellipseConv.py
@@ -1,8 +1,8 @@
 import Utilities.fileIO as fIO
 import Utilities.coordSystems as coords
 
-blockNames, xyzVals = fIO.loadXYZ('spidroinHairpinTest.xyz')
+blockNames, xyzVals = fIO.loadXYZ('hPin.xyz')
 eAtomNames, ePositions, eSizes, eRs, eRotVecs = coords.convertTriplesToEllipsoids(blockNames, xyzVals, 1.0, 1.0)
-fIO.saveEllipsoidXYZList(eAtomNames, ePositions, eSizes, eRs, eRotVecs, 'spidroinHairpinTest.e.xyz')
+fIO.saveEllipsoidXYZList(eAtomNames, ePositions, eSizes, eRs, eRotVecs, 'hPin.e.xyz')
 
 print "example done"
